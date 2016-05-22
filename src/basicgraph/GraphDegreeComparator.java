408322a8bbc42d1ba9e9371b7eb3c5ed95877fad
Mahendra Chhimwal <cyberpash@gmail.com>
2016-05-22 19:49:24 +0530

    first commit

---
+++ src/basicgraph/GraphDegreeComparator.java
@@ -0,0 +1,10 @@
+package basicgraph;
+
+import java.util.Comparator;
+
+public class GraphDegreeComparator implements Comparator<Integer>{
+	@Override
+	public int compare(Integer o1, Integer o2) {		
+		return o2.compareTo(o1);
+	}	
+}
